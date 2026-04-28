36b03841814ace9fe449995b0aa584381ba4f210
zpznnc449 <hedge.timbres72@icloud.com>
2026-04-28 16:00:25 +0300

    Update task_4-12-1.py

--- project_4/task_4-12-1.py
+++ project_4/task_4-12-1.py
@@ -2,13 +2,11 @@ array = [7, 3, 8, 1, 4, 6, 2, 5]
 n = len(array)
 
 for i in range(n):
-    swapped = False  # Флаг: была ли перестановка?
+    swapped = False  
     for j in range(0, n - i - 1):
         if array[j] > array[j + 1]:
             array[j], array[j + 1] = array[j + 1], array[j]
             swapped = True 
-            
-    # Если за весь проход не было перестановок — выходим
     if not swapped:
         break
 
